bc52c2abe2e9e8f6d053835387cf16a72591a78e
shajf <csp001314@163.com>
2014-05-28 10:28:02 +0800

    jianfeng sha add ngx_security_action

---
+++ ngx_modsecurity/modsecurity-1.0.1/src/ngx_security_action.h
@@ -0,0 +1,19 @@
+/*
+ * =====================================================================================
+ *      Copyright (C) 2014 jianfeng sha
+ *
+ *      Filename:  ngx_security_action.h
+ *
+ *      Description:  
+ *
+ *      Created:  05/28/14 10:25:58
+ *
+ *      Author:  jianfeng sha , csp001314@163.com
+ * =====================================================================================
+ */
+#ifndef NGX_SECURITY_ACTION_H
+#define NGX_SECURITY_ACTION_H
+
+
+
+#endif /*NGX_SECURITY_ACTION_H*/
